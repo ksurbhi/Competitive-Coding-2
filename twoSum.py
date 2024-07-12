8379ac3201136799aea794ebd8930edf9280963a
ksurbhi <94086790+ksurbhi@users.noreply.github.com>
2024-07-12 14:53:45 -0700

    Two Sum

---
+++ twoSum.py
@@ -0,0 +1,12 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        myDict = {}
+        for i in range(len(nums)):
+            if target-nums[i] in myDict:
+                return [myDict[target-nums[i]],i]
+            else:
+                myDict[nums[i]] = i
+
+# Time Complexity: O(n)
+# Space Complexity: O(n)
+        
